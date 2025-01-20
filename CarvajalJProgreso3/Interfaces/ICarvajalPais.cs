93f796796f36d468e4b9fd259c6bbe0625b6ee06
JoseCarvajal1264 <jose.carvajal@udla.edu.ec>
2025-01-20 07:57:54 -0500

    interfaces

---
+++ CarvajalJProgreso3/Interfaces/ICarvajalPais.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarvajalJProgreso3.Models;
+
+
+namespace CarvajalJProgreso3.Interfaces
+{
+    internal interface ICarvajalPais
+    {
+        public Task<List<CarvajalPaisAPI>> Obtener();
+    }
+}
